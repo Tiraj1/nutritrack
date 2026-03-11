e10fe332750b01d3a99696854b1352fd9f92b175
Tiraj1 <tirajp98@gmail.com>
2026-03-11 10:14:50 +0000

    Fix Turbopack build conflict for Vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const withPWA = require("next-pwa")({
- dest:"public",
- disable: process.env.NODE_ENV === "development"
+  dest: "public",
+  disable: process.env.NODE_ENV === "development"
 })
 
 module.exports = withPWA({
- reactStrictMode:true
+  reactStrictMode: true,
+
+  // Prevent Turbopack conflict
+  turbopack: {}
 })
